f082b81018e74ae90499a6a222d938d2ab0534af
17702858513 <1059784729@qq.com>
2023-07-14 17:43:58 +0800

    feat:更新打包依赖

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
 import typescript from 'rollup-plugin-typescript2'
-import path from 'path'
 import { terser } from 'rollup-plugin-terser'
+import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
 
 export default {
 	input: './src/index.ts',
-	output: {
-		file: path.resolve(__dirname, './dist/index.js'),
-		format: 'umd',
-		name: 'toolkit',
-		sourcemap: false
-	},
-	plugins: [typescript(), terser(), resolve(['.js', '.ts'])]
+	output: [
+		{
+			file: 'dist/index.js',
+			format: 'cjs'
+		}
+	],
+	plugins: [typescript(), terser(), commonjs(), resolve(['.js', '.ts'])]
 }
