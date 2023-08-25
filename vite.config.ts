43b551a95b05ce95cb355a5bde874e1bd2cc51cc
17702858513 <1059784729@qq.com>
2023-08-25 18:08:57 +0800

    feat: ts支持

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [vue(), dts({ rollupTypes: true })],
 	build: {
+		outDir: 'dist',
 		lib: {
-			entry: './src/index.ts',
-			formats: ['es', 'cjs'],
+			//指定组件编译入口文件
+			entry: path.resolve(__dirname, 'src/index.ts'),
+			name: 'index',
 			fileName: 'index'
 		},
 		rollupOptions: {
+			// 确保外部化处理那些你不想打包进库的依赖
 			external: ['vue'],
-			output: [
-				{
-					dir: 'dist',
-					format: 'es' // ESM 格式
-				},
-				{
-					format: 'cjs', // CJS 格式
-					dir: 'dist' // 输出目录
+			output: {
+				// 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+				globals: {
+					vue: 'Vue'
 				}
-			]
+			}
 		}
 	}
 })
